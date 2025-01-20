5e4c34c870278fc1e243fe268d7d16445965dd29
ozztec <76617397+ozztec@users.noreply.github.com>
2025-01-20 22:57:17 +0300

    Update Gemini.js

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -10,7 +10,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = "Your Api Key ";
+const API_KEY = "AIzaSyAHHPYCoY9QtP6KSBHPHxjSbWtLVVNqyGo ";
 
 async function runChat(prompt) {
   const genAI = new GoogleGenerativeAI(API_KEY);
